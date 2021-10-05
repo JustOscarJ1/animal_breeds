c25863390aec3230d14fbca4ac8d4d20ebd72c09
JustOscarJ1 <30467017+JustOscarJ1@users.noreply.github.com>
2021-10-06 09:59:16 +1100

    main files
    main files

---
+++ main.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup # pip install bs4
+import requests # pip install requests
+
+request_data = requests.get('https://en.wikipedia.org/wiki/List_of_dog_breeds').text # Makes a request to the website and gets the HTML data by accessing the .text attribute of the request.
+soup = BeautifulSoup(request_data, features="html.parser") # Uses the HTML data to make soup.
+
+for i in soup.select('div[class="div-col"] >ul > li > a[href^="/wiki/"]'): # Grabs every element with this specific CSS selector then iterates over the elements.
+    print(i.text) # Prints the text within the element
